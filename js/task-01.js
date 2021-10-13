fa8210ce75c3c82f8b89d49bb59b59624d874bb7
Vadym-Kuliabko <vadilatele@gmail.com>
2021-10-13 21:45:56 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const listEl = document.querySelector("#categories");
+
+const itemsListEl = document.querySelectorAll(".item");
+console.log("number of categories:", itemsListEl.length);
+
+itemsListEl.forEach((element) => {
+  element.firstElementChild.textContent;
+  element.lastElementChild.children.length;
+  const title = element.querySelector("h2").innerHTML;
+  const itemsLength = element.querySelectorAll("li").length;
+
+  console.log(`category: ${title} / elements : ${itemsLength}`);
+});
