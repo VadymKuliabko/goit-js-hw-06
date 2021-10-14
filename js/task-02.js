97abb7b1426e0ec306ea9109cfbe405f70fbfe24
Vadym-Kuliabko <vadilatele@gmail.com>
2021-10-14 13:45:10 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+console.log(ingredients);
+
+const listEl = document.querySelector("#ingredients");
+ingredients.forEach((element) => {
+  const itemEl = document.createElement("li");
+  // console.log(itemEl);
+  itemEl.textContent = element;
+  itemEl.classList.add("item");
+  listEl.append(itemEl);
+  console.log(itemEl);
+});
