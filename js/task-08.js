8d0c8e9801229b510250ff26db80adcc9c739b99
Vadym-Kuliabko <vadilatele@gmail.com>
2021-10-18 17:06:21 +0200

    8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
 
+const onSubmitForm = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Заполните пожалуйста все поля");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+formEl.addEventListener("submit", onSubmitForm);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorEl = document.querySelector(".change-color");
+const bodyColorEl = document.querySelector(".color");
+
+const onBodyColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  bodyColorEl.textContent = `${getRandomHexColor()}`;
+
+  console.log(getRandomHexColor());
+};
+
+changeColorEl.addEventListener("click", onBodyColor);
